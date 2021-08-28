ab0cc3ddf2d0039df48ae3fc015cb4179b52e281
KimYuJin <kimyujin1211@gmail.com>
2021-08-29 02:09:30 +0900

    Add Bp, C++ Class

---
+++ Source/PreTest/ProjectileReflect.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseProjectile.h"
+#include "ProjectileReflect.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRETEST_API AProjectileReflect : public ABaseProjectile
+{
+	GENERATED_BODY()
+	
+};
