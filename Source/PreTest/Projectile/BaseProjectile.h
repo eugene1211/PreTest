8df171490f14410fe5fb2f353c4ae93571e9ef92
KimYuJin <kimyujin1211@gmail.com>
2021-08-30 01:34:04 +0900

    - 불필요한 주석 제거

--- Source/PreTest/Projectile/BaseProjectile.h
+++ Source/PreTest/Projectile/BaseProjectile.h
@@ -29,7 +29,6 @@ public:
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	//void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 	
 	bool bDestroyedByHit;
 
